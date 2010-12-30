98f14162ae109f3369784c99973a1aa4a91d57df
elzoiddy <xzhang@rnmd.net>
2010-12-30 14:19:28 -0800

    added configureable option to set the reason for update as :none, :optional and :required.

--- lib/vestal_versions/comments.rb
+++ lib/vestal_versions/comments.rb
@@ -1,25 +1,53 @@
 module VestalVersions
+  
+  class CommentForUpdateRequired < RuntimeError; end
+  
   # Provides a way for comments/reason for update to be associated with specific 
   # versions
   
   module Comments
-    def self.included(base) # :nodoc:
-      base.class_eval do
-        include InstanceMethods
-        attr_accessor :reason_for_update
+    extend ActiveSupport::Concern
+
+    included do
+      attr_accessor :reason_for_update
+    end
+
+
+    module ClassMethods
+      # After the original +prepare_versioned_options+ method cleans the given options, this alias
+      # also extracts the <tt>:update_comments</tt> options
+      # 
+      # If the <tt>:update_comments</tt> is not set or set to :none, no reason for 
+      # update will be stored.  If the option is set to :required, an exception 
+      # will be thrown if reason for update is missing when updating a record.
+      # Any other value is treated as :optional. which means reason will only be stored 
+      # if one is supplied. 
+      
+      def prepare_versioned_options(options)
+        result = super(options)
+        vestal_versions_options[:update_comments] = options.delete(:update_comments)
+        result
       end
     end
 
     # Methods added to versioned ActiveRecord::Base instances to enable versioning with 
     # additional column to store a comment or reason for updating
-
-    module InstanceMethods
-      private
-        # Overrides the +version_attributes+ method to include a comment passed into the
-        # parent object, by way of a +reason_for_update+ attr_accessor.
-        def version_attributes
-          super.merge(:reason_for_update => reason_for_update)
+    private
+      # Overrides the +version_attributes+ method to include a comment passed into the
+      # parent object, by way of a +reason_for_update+ attr_accessor.
+      def version_attributes
+        comments_option = vestal_versions_options[:update_comments]
+        # :none or not present
+        if comments_option.blank? || comments_option == :none
+          return super 
         end
-    end
+        
+        if comments_option == :required && reason_for_update.blank?
+          raise CommentForUpdateRequired.new("Need a comment or reason for updating record.")
+        end
+         
+        super.merge(:reason_for_update => reason_for_update)
+      end
+
   end
 end
--- spec/vestal_versions/comments_spec.rb
+++ spec/vestal_versions/comments_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe VestalVersions::Comments do
   let(:user){ User.create(:name => 'Steve Richert') }
+  
+  before do
+    User.prepare_versioned_options(:update_comments => :optional)
+  end
 
   it 'defaults to nil' do
     user.update_attributes(:first_name => 'Stephen')
@@ -11,7 +15,36 @@ describe VestalVersions::Comments do
   it 'accept and return a reason for update' do
     user.update_attributes(:first_name => 'Stephen', 
       :reason_for_update => "some reason", :updated_by => "me" )
+    
     user.versions.last.reason_for_update.should == "some reason"
     user.versions.last.user_name.should == 'me'
   end
+  
+  it "should require a reason for update when configured" do
+    User.prepare_versioned_options(:update_comments => :required)
+    lambda {user.update_attributes(:first_name => 'Stephen')}.should raise_error
+
+    user.update_attributes(:first_name => 'Stephen', 
+      :reason_for_update => "some reason")
+
+    user.versions.last.reason_for_update.should == "some reason"
+  end
+
+  it "should not record a reason for update when not configured" do
+    # not configured at all
+    User.prepare_versioned_options(:update_comments => nil)
+    user.update_attributes(:first_name => 'Stephen', 
+      :reason_for_update => "some reason")
+
+    user.versions.last.reason_for_update.should be_nil
+
+    # configured for :none
+    User.prepare_versioned_options(:update_comments => :none)
+    user.update_attributes(:first_name => 'Stephen', 
+      :reason_for_update => "some reason")
+
+    user.versions.last.reason_for_update.should be_nil
+  end
+
+  
 end
